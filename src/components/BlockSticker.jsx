b986043ebf22d5c766a73e5000cfe97de8eaf75a
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2022-01-23 08:36:24 +0600

    z index

---
+++ src/components/BlockSticker.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BlockSticker = () => {
+    return <div className="container-two-1">
+
+        <img src="https://ik.imagekit.io/ikmedia/accessories_banner.jpg?tr=w-1200,h-400" alt="snow" width="100%" />
+        {/* <img src="https://picsum.photos/1400/500" alt="snow" width="100%" /> */}
+        <div className="content-1">
+            <h1>Heading</h1>
+            <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei.</p>
+        </div>
+        </div>;
+};
+
+export default BlockSticker;
---
+++ src/components/ZindexImageOverImage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ZindexImageOverImage = () => {
+  return <div>
+    <div>
+         
+
+          <img src="https://picsum.photos/1400/400" className="fishes" />
+          <img src="https://picsum.photos/100/100"className="fish" />
+</div>
+</div>;
+};
+
+export default ZindexImageOverImage;
